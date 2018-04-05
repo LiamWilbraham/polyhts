113a1207ab9d0f2031cb0eed4f3efcd6b80a87e2
Liam Wilbraham <liam@chemb137.chem.ucl.ac.uk>
2018-04-05 18:05:02 +0100

    increased user functionality

--- POLYHTS-v3.py
+++ POLYHTS-v3.py
@@ -17,6 +17,13 @@ Before the calculation of the above properties, a confomer search is conducted s
 Experimental Torsion Distance Geometry with additional basic Knowledge (ETKDG) [1] approach. On the resulting
 lowest-energy confomer, structure optimisation and electronic properties are calculated using a family of 
 semi-empirical methods based on the GFN-xTB density functional tight binding method proposed by Grimme [2-4].
+By default, copolymers are modelled using a linear chain of 12 monomer units. Solvation effects can be included
+using the gbsa solvation model associated with GFN-xTB.
+
+Additonally, one may provide parameters to calibrate the computed data. For example, for a particular problem
+you might want to calibrate the semi-empirically derived properties to those computed using a higher-level 
+electronic structure method. This can be done automatically by providing linear regression fit parametes
+(i.e. slopes and intercepts) to the script (see below).
 
 references
 ----------
@@ -25,17 +32,33 @@ references
  [3] Comput. Theor. Chem. 2014, 1040, 45-53 
  [4] J. Chem. Phys. 2016, 145, 054103
 
-----------------------------------------------------------
+--------------------------------------------------------
 Input parameters for high throughput screening procedure
-----------------------------------------------------------
+--------------------------------------------------------
 
-    nconfs      : Number of confomers generated in stochastic confomer search
-                  By default, this is set to 1000
+    nconfs         : Number of confomers generated in stochastic confomer search
+                     By default, this is set to 1000
 
-    monomer_dir : Directory containing .mol files of monomer units used to construct copolymers
-                  By default, this is set to a directory 'monomers' within the working directory
+    monomer_dir    : Directory containing .mol files of monomer units used to construct copolymers
+                     By default, this is set to a directory 'monomers' within the working directory
 
-    num_cores   : Number of cores used in (embarrassingly) parallel polymer screening
+    xtb_dir        : Full path to directory containing the executable for the xtb program 
+
+    num_cores      : Number of cores used in (embarrassingly) parallel polymer screening
+
+    candidate_list : Name of a file containing a list of copolymer candidates. The candidate list file
+                     must be written in the following format:
+
+                     Unit 1    Unit 2    ID
+                     XXXX      YYYY      ZZZZ
+
+                     where XXXX and YYYY and four digit reference numbers corresponding to the monomer
+                     units that will make up a copolymer with ID number ZZZZ.           
+
+    solvent        : Choose the solvent model parameters to be used. Available parameters are within 
+                     the gbsa solvation model in GFN-xTB (acetone, acetonitrile, benzene, chcl3, cs2,
+                     dmso, ether, h2o, methanol, thf, toluene)
+ 
 
     Linear calibration parameters :
         - ip_intercept      
@@ -48,24 +71,27 @@ Input parameters for high throughput screening procedure
         By default these are set to 1 and 0 for slopes and intercepts, respectively (i.e. no calibration)
 
 '''
-
-nconfs = 1000
+# Input Parameters 
+#---------------------------------------------
+nconfs = 10
 monomer_dir = os.getcwd()+'/monomers/'
+xtb_dir = '/home/liam/software/XTB'
 num_cores = 20
+candidate_list = 'candidate_list.dat' 
+solvent = 'h2o'
 ip_intercept = 0.
 ip_slope = 1.
 ea_intercept = 0.
 ea_slope = 1.
 opt_gap_intercept = 0.
 opt_gap_slope = 1.
-
-
+#---------------------------------------------
 
 # Generates a polymer with MTK
 def generate_polymer(unit1, unit2, length, sequence, name, monomer_dir):
 
     A = mtk.StructUnit2(monomer_dir+unit1, "bromine")
-    B = mtk.Unit2(monomer_dir+unit2, "bromine")
+    B = mtk.StructUnit2(monomer_dir+unit2, "bromine")
     polymer = mtk.Polymer([A,B], mtk.Linear(sequence, [1,1], n=int(length/len(sequence))), name=name)
     polymer.write(name+'.mol')
 
@@ -108,19 +134,19 @@ def confomer_search(nconfs, name):
 
 
 # Optimisation of lowest energy confomer with GFN-xTB
-def confomer_opt(lowest, id, name):
+def confomer_opt(lowest, id, name, solvent):
 
     os.system('babel '+id+'.mol '+id+'.xyz')
-    os.system('xtb '+id+'.xyz -opt -gbsa h2o > '+id+'-opt.out')    
+    os.system('xtb '+id+'.xyz -opt -gbsa '+solvent+' > '+id+'-opt.out')    
     os.system('mv xtbopt.xyz '+id+'-opt.xyz')
 
     return id
 
 
 # Calculation of IP with GFN-xTB using IP/EA fitted parameters
-def confomer_ip(id, name):
+def confomer_ip(id, name, solvent):
 
-    os.system('xtb '+id+'-opt.xyz -vip -gbsa h2o > '+id+'-ip.out')
+    os.system('xtb '+id+'-opt.xyz -vip -gbsa '+solvent+' > '+id+'-ip.out')
 
     with open(id+'-ip.out','r') as ip_out:
         inlines = [line.strip() for line in ip_out]
@@ -132,9 +158,9 @@ def confomer_ip(id, name):
 
 
 # Calculation of EA with GFN-xTB using IP/EA fitted parameters
-def confomer_ea(id, name):
+def confomer_ea(id, name, solvent):
    
-    os.system('xtb '+id+'-opt.xyz -vea -gbsa h2o > '+id+'-ea.out')
+    os.system('xtb '+id+'-opt.xyz -vea -gbsa '+solvent+' > '+id+'-ea.out')
 
     with open(id+'-ea.out','r') as ea_out:
         inlines = [line.strip() for line in ea_out]
@@ -170,9 +196,9 @@ def opt_gap_linearfit_convert(opt_gap, opt_gap_intercept, opt_gap_slope):
 
 
 # Calculate optical gap and oscillator strength with sTDA-xTB
-def stda(id, name):
+def stda(id, name, solvent):
 
-    os.system('xtb '+id+'-opt.xyz -gbsa h2o > '+id+'-wfn.out')
+    os.system('xtb '+id+'-opt.xyz -gbsa '+solvent+' > '+id+'-wfn.out')
     os.system(' cp wfn.xtb '+id+'-stda.wfn')
     os.system('stda -xtb -e 10 > '+id+'-stda.out')
 
@@ -205,9 +231,14 @@ def solv_energy(id, name):
 # Read in list of polymer candidates from candidate list
 def read_candidates(candidate_list):
 
-    with open(candidate_list, 'r') as candidates:
-        candidate_list = [line.split() for line in candidates]        
-        candidate_list = candidate_list[1:]
+    try:
+        with open(candidate_list, 'r') as candidates:
+            candidate_list = [line.split() for line in candidates]        
+            candidate_list = candidate_list[1:]
+
+    except FileNotFoundError:
+        print('ERROR : no list of candidates provided')
+        exit()
 
     return candidate_list
 
@@ -226,10 +257,10 @@ def main(unit1, unit2, tag, nconfs, monomer_dir):
  
         lowest, id = confomer_search(nconfs, name)
 
-        id = confomer_opt(lowest, id, name)
-        ip = ip_linearfit_convert(confomer_ip(id, name), ip_intercept, ip_slope)[:7]
-        ea = ea_linearfit_convert(confomer_ea(id, name), ea_intercept, ea_slope)[:7]
-        opt_gap, osc_strength = stda(id, name)
+        id = confomer_opt(lowest, id, name,solvent)
+        ip = ip_linearfit_convert(confomer_ip(id, name, solvent), ip_intercept, ip_slope)[:7]
+        ea = ea_linearfit_convert(confomer_ea(id, name, solvent), ea_intercept, ea_slope)[:7]
+        opt_gap, osc_strength = stda(id, name, solvent)
         opt_gap = opt_gap_linearfit_convert(opt_gap, opt_gap_intercept, opt_gap_slope)[:7]
         e_solv = str(solv_energy(id, name))[:7]
 
@@ -251,8 +282,13 @@ def main(unit1, unit2, tag, nconfs, monomer_dir):
 #--------------------------------------------------------------------------------------------------
 # Run polymer screening in parallel with number of parallel threads = num_cores
 if __name__ == "__main__":
-    candidates = read_candidates('candidate_list.dat')
+    candidates = read_candidates(candidate_list)
+    os.environ['XTBHOME']= xtb_dir
     with open('screened-polymers.dat', 'w') as screened:
         screened.write('Unit1       Unit2       ID          IP (eV)     EA (eV)     Opt. Gap (eV)       Osc. Strength     Solvation Energy(eV)\n')
-    results = Parallel(n_jobs=num_cores)(delayed(main)(unit1, unit2, tag, nconfs, monomer_dir) for unit1, unit2, tag in candidates)
+    try:
+        results = Parallel(n_jobs=num_cores)(delayed(main)(unit1, unit2, tag, nconfs, monomer_dir) for unit1, unit2, tag in candidates)
+    except:
+        print('ERROR : perhaps the candidate list file is in an invalid format')
+        exit()
 #--------------------------------------------------------------------------------------------------
