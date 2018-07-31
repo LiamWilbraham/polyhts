048586e494c5d88b93454c2b09dbd73b94a2b02f
Liam Wilbraham <liam@chemb137.chem.ucl.ac.uk>
2018-07-31 19:13:15 +0100

    added standalone usable functions

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ valid_solvents = ['acetone', 'acetonitrile', 'benzene', 'chcl3', 'cs2',
 
 output_header = 'ID1\tID2\tIP\tEA\tS0->S1\tF\tSmiles1\tSmiles2\n'
 
+
 @contextmanager
 def cd(newdir):
     prevdir = os.getcwd()
@@ -16,15 +17,18 @@ def cd(newdir):
     finally:
         os.chdir(prevdir)
 
+
 def run_calc(calc_params):
     p = sp.Popen(calc_params, stdout=sp.PIPE, encoding='utf8')
     output, _ = p.communicate()
     return output
 
+
 def property_log(id1, id2, smiles1, smiles2, vip, vea, gap, f):
     with open('../screening-output', 'a+') as output:
         output.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(id1, id2, vip, vea, gap, f, smiles1, smiles2))
 
+
 def error_log(id1, id2, smiles1, smiles2, e):
     with open('../screening-output', 'a+') as output:
-        output.write('{}\t{}\t{}\t{}\t{}\n'.format(id1, id2, smiles1, smiles2, e))
+        output.write('{}\t{}\t{}\t{}\tERROR:{}\n'.format(id1, id2, smiles1, smiles2, e))
